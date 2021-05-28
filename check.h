ea71d9e186e90c7fd3f1d23d513f92c2ab43e8b6
Kim Da Hyung <77934734+korpanda@users.noreply.github.com>
2021-05-28 21:14:12 +0800

    Create check.h

---
+++ check.h
@@ -0,0 +1,29 @@
+//check.h
+
+#ifndef CHECK
+#define CHECK
+
+using namespace std;
+
+class Check{
+public:
+  bool IsFourOfAKind(int cards[]);
+
+  bool IsFullHouse(int cards[]);
+
+  bool IsFlush(int cards[]);
+
+  bool IsThreeOfAKind(int cards[]);
+
+  int countPair(int cards[]);
+
+  int getNoPair(int cards[]);
+
+  bool IsTwoPair(int cards[]);
+
+  bool IsOnePair(int cards[]);
+
+};
+
+
+#endif
